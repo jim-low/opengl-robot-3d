b9914b70fc9f34b4e98f3989f3b67e7372a13f1d
jim-low <robil12380@gmail.com>
2022-12-30 11:56:36 +0800

    feat: add Legs class

--- Graphics Programming Assignment/main.cpp
+++ Graphics Programming Assignment/main.cpp
@@ -52,6 +52,9 @@ bool initPixelFormat(HDC hdc)
 
 void display()
 {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glLoadIdentity();
 }
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
