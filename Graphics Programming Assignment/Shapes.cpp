e418919d3985bad20f90fe1af6b6a89fe88b63aa
jim-low <robil12380@gmail.com>
2022-12-31 23:46:44 +0800

    fix: add back draw cube function

--- Graphics Programming Assignment/Shapes.cpp
+++ Graphics Programming Assignment/Shapes.cpp
@@ -1,7 +1,7 @@
 #include "Shapes.h"
 
 void Shapes::drawCylinder(GLdouble baseRadius, GLdouble topRadius, GLdouble height, GLint slices, GLint stacks, GLenum drawStyle) {
-	
+
 
 	GLUquadricObj* gluObj = NULL;//create quadric obj pointer
 	gluObj = gluNewQuadric();
@@ -14,7 +14,7 @@ void Shapes::drawCylinder(GLdouble baseRadius, GLdouble topRadius, GLdouble heig
 }
 
 void Shapes::drawSphere(GLdouble radius, GLint slices, GLint stacks, GLenum drawStyle) {
-	
+
 
 	GLUquadricObj* gluObj = NULL;//create quadric obj pointer
 	gluObj = gluNewQuadric();
@@ -26,7 +26,7 @@ void Shapes::drawSphere(GLdouble radius, GLint slices, GLint stacks, GLenum draw
 
 }
 
-void Shapes::drawCube(Vector3 startPoint, float width, float height, float length) {
+void Shapes::drawCuboid(Vector3 startPoint, float width, float height, float length) {
 	// front face
 	glBegin(GL_LINE_LOOP);
 	glVertex3f(startPoint.x, startPoint.y, startPoint.z);
@@ -50,7 +50,7 @@ void Shapes::drawCube(Vector3 startPoint, float width, float height, float lengt
 	glVertex3f(startPoint.x + width, startPoint.y + height, startPoint.z + length);
 	glVertex3f(startPoint.x + width, startPoint.y + height, startPoint.z);
 	glEnd();
-	
+
 	// top side of face
 	glBegin(GL_LINE_LOOP);
 	glVertex3f(startPoint.x, startPoint.y + height, startPoint.z);
@@ -74,4 +74,38 @@ void Shapes::drawCube(Vector3 startPoint, float width, float height, float lengt
 	glVertex3f(startPoint.x + width, startPoint.y + height, startPoint.z + length);
 	glVertex3f(startPoint.x, startPoint.y + height, startPoint.z + length);
 	glEnd();
+}
+
+void Shapes::drawCube(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4, Vector3 p5, Vector3 p6, Vector3 p7, Vector3 p8)
+{
+	glVertex3f(p1.x, p1.y, p1.z);		//	point 1
+	glVertex3f(p2.x, p2.y, p2.z);		//	point 2
+	glVertex3f(p3.x, p3.y, p3.z);		//	point 3
+	glVertex3f(p3.x, p3.y, p3.z);		//	point 3
+	glVertex3f(p4.x, p4.y, p4.z);		//	point 4
+
+	glVertex3f(p5.x, p5.y, p5.z);		//	point 5
+	glVertex3f(p6.x, p6.y, p6.z);		//	point 6
+	glVertex3f(p7.x, p7.y, p7.z);		//	point 7
+	glVertex3f(p8.x, p8.y, p8.z);		//	point 8
+
+	glVertex3f(p1.x, p1.y, p1.z);		//	point 1
+	glVertex3f(p5.x, p5.y, p5.z);		//	point 5
+	glVertex3f(p6.x, p6.y, p6.z);		//	point 6
+	glVertex3f(p2.x, p2.y, p2.z);		//	point 2
+
+	glVertex3f(p4.x, p4.y, p4.z);		//	point 4
+	glVertex3f(p8.x, p8.y, p8.z);		//	point 8
+	glVertex3f(p7.x, p7.y, p7.z);		//	point 7
+	glVertex3f(p3.x, p3.y, p3.z);		//	point 3
+
+	glVertex3f(p1.x, p1.y, p1.z);		//	point 1
+	glVertex3f(p5.x, p5.y, p5.z);		//	point 5
+	glVertex3f(p8.x, p8.y, p8.z);		//	point 8
+	glVertex3f(p4.x, p4.y, p4.z);		//	point 4
+
+	glVertex3f(p2.x, p2.y, p2.z);		//	point 2
+	glVertex3f(p6.x, p6.y, p6.z);		//	point 6
+	glVertex3f(p7.x, p7.y, p7.z);		//	point 7
+	glVertex3f(p3.x, p3.y, p3.z);		//	point 3
 }
--- Graphics Programming Assignment/Shapes.h
+++ Graphics Programming Assignment/Shapes.h
@@ -12,6 +12,7 @@ class Shapes
 public:
 	static void drawCylinder(GLdouble baseRadius, GLdouble topRadius, GLdouble height, GLint slices, GLint stacks, GLenum drawStyle);
 	static void drawSphere(GLdouble radius, GLint slices, GLint stacks, GLenum drawStyle);
-	static void drawCube(Vector3 startPoint, float width, float height, float length);
+	static void drawCuboid(Vector3 startPoint, float width, float height, float length);
+	static void drawCube(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4, Vector3 p5, Vector3 p6, Vector3 p7, Vector3 p8);
 };
 
