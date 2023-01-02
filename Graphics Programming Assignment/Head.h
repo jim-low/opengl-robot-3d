e8e78db5579f3b1531c381c502757902a93326a9
Zi Heng <ziheng3123@gmail.com>
2023-01-02 22:40:39 +0800

    complete robot with head

--- Graphics Programming Assignment/Head.h
+++ Graphics Programming Assignment/Head.h
@@ -1,8 +1,24 @@
+#include <Math.h>
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include "Vector3.h"
+#include "Shapes.h"
+
+#pragma comment (lib, "OpenGL32.lib")
+#pragma comment (lib, "GLU32.lib")
 
 #pragma once
 class Head
 {
 public:
 	void draw();
+	void drawFrontPlane();
+	void drawFrontTopPlane();
+	void drawFrontBottomPlane();
+	void drawBackHalf();
+	void drawNeck();
+	void drawAntenna1();
+	void drawAntenna2();
 };
 
